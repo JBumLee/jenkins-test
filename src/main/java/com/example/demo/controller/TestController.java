db7f4b37686b288ccd87c74a20303fee38166d80
LeeJungBum <jungbum05114@gmail.com>
2024-02-13 14:47:48 +0900

    test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -15,5 +15,10 @@ public class TestController {
     public String test(){
         return "test";
     }
+
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
+    }
 }
 
