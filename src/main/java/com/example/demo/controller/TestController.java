2701e759f60237cb3c3028a315548cd62a7940dd
LeeJungBum <jungbum05114@gmail.com>
2024-02-13 13:18:09 +0900

    test

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * class: TestController.
+ *
+ * @author ijeongbeom
+ * @version 2024/02/13
+ */
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test(){
+        return "test";
+    }
+}
+
